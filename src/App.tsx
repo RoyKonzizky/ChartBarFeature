ae993f7f11c1db70896741abffafef53c4059cdb
Roy Konzizky <roeicop110@gmail.com>
2023-11-21 16:00:47 +0200

    -draws annotation.

--- src/App.tsx
+++ src/App.tsx
@@ -48,17 +48,44 @@ function App() {
             }
             else{
                 difference = cutBarsTooltipItems[1].parsed.y - cutBarsTooltipItems[0].parsed.y;
-            }
+             }
         }
 
         return 'Difference: ' + difference;
     };
 
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-expect-error
-    const barFilter = (tooltipItems) => {
-        return [tooltipItems[0], tooltipItems[1]];
-    }
+    // // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // // @ts-expect-error
+    // const barFilter = (tooltipItems) => {
+    //     return [tooltipItems[0], tooltipItems[1]];
+    // }
+
+    const plugins = [
+        {
+            afterDraw: (chart: { tooltip?: any; scales?: any; ctx?: any }) => {
+                // eslint-disable-next-line no-underscore-dangle
+                if (chart.tooltip._active && chart.tooltip._active.length) {
+                    // find coordinates of tooltip
+                    const activePoint = chart.tooltip._active[0];
+                    const { ctx } = chart;
+                    const { x } = activePoint.element;
+                    const topY = chart.scales.y.top;
+                    const bottomY = chart.scales.y.bottom;
+
+                    // draw vertical line
+                    ctx.save();
+                    ctx.beginPath();
+                    ctx.moveTo(x, topY);
+                    ctx.lineTo(x, bottomY);
+                    ctx.lineWidth = 1;
+                    ctx.strokeStyle = '#1C2128';
+                    ctx.stroke();
+                    ctx.restore();
+                }
+            },
+        },
+    ];
+
 
     const options = {
         responsive: true,
@@ -82,7 +109,10 @@ function App() {
                 //     callbacks: {
                 //         footer: barFilter
                 //     }
-                // }
+                // },
+                // content:{
+                //     filter:"bar",
+                // },
             },
         },
     };
@@ -105,32 +135,32 @@ function App() {
         ],
     };
 
-    const barDatasets = labels.map((label, index) => {
-        return {
-            label: null,
-            type: 'bar',
-            data: [
-                {
-                    x: label,
-                    y: [dataLabels1[index], dataLabels2[index]],
-                },
-            ],
-            borderColor: 'rgb(0, 255, 0)',
-            backgroundColor: 'rgb(0, 255, 0)',
-            borderWidth: 0.1,
-            barThickness: 0.1,
-        };
-    });
-
-    const updatedDatasets = [...data.datasets, ...barDatasets];
-
-    const updatedData = {
-        ...data,
-        datasets: updatedDatasets,
-    };
+    // const barDatasets = labels.map((label, index) => {
+    //     return {
+    //         label: null,
+    //         type: 'bar',
+    //         data: [
+    //             {
+    //                 x: label,
+    //                 y: [dataLabels1[index], dataLabels2[index]],
+    //             },
+    //         ],
+    //         borderColor: 'rgb(0, 255, 0)',
+    //         backgroundColor: 'rgb(0, 255, 0)',
+    //         borderWidth: 0.1,
+    //         barThickness: 0.1,
+    //     };
+    // });
+    //
+    // const updatedDatasets = [...data.datasets, ...barDatasets];
+    //
+    // const updatedData = {
+    //     ...data,
+    //     datasets: updatedDatasets,
+    // };
 
     return (
-        <Line style={styleChart} options={options} data={updatedData}/>
+        <Line style={styleChart} plugins={plugins} options={options} data={data}/>
     );
 }
 
